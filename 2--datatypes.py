670bac46f7d6ed97898b1b1193baa35c12402bba
Naga kalyan Nukala <kalyannukala768@gmail.com>
2024-04-02 17:27:28 +0000

    ExampleClassTask

---
+++ 2--datatypes.py
@@ -0,0 +1,6 @@
+sample_str = "The cat is running around the streets"
+
+print(sample_str.startswith('cat', 4, 7))
+print(sample_str.endswith('cat', 8, 11))
+print(sample_str.index('cat'))
+print(sample_str.find('the'))
